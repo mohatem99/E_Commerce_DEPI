9074aa359356ba3f93baddc6469c857716e4e8a0
Omar Araby <omararaby789@gmail.com>
2024-08-24 07:05:29 +0300

    Handle login design

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <form class="max-w-sm mx-auto">
+        <div class="mb-5">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            class="bg-gray-50 border border-cyan-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-cyan-700 dark:border-cyan-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500"
+            placeholder="name@flowbite.com"
+            required
+          />
+        </div>
+        <div class="mb-5">
+          <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            class="bg-gray-50 border border-cyan-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="*******"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          class="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
 }
